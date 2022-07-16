c775beef8ed84192a67c8e98f0ebb771dd7b7e4d
Atlas MERCAN <atlasmercan99@gmail.com>
2022-07-16 18:56:13 +0300

    init

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+const App = () => {
+  return <SafeAreaView />;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const styles = StyleSheet.create({});
+
+export default App;
